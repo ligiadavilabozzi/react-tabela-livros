7e2f7f47806dc16db7951f95d82e9291d803e7e1
ligiadavilabozzi <ligia.bozzi@gmail.com>
2021-05-15 17:52:34 -0300

    create dinamic requisition with json

--- src/App.js
+++ src/App.js
@@ -5,24 +5,19 @@ import TabelaBody from './components/TabelaBody'
 
 class App extends Component {
   state={
-    livros:[
-      {
-        id: "978-85-7522-632-2", 
-        titulo: "CSS Grid Layout",
-        autor: "Maurício Samy Silva"
-      },
-      {
-        id: "978-85-7522-677-3", 
-        titulo: "Node Essencial",
-        autor: "Ricardo R. Lacheta"
-      },
-      {
-        id: "978-85-7522-512-7", 
-        titulo: "Aprendendo Material Design",
-        autor: "Kyle Mew"
-      }
-    ]
+    livros:[]
   };
+  componentDidMount(){
+    fetch("/api/livros.json")
+    .then(response => response.json())
+    .then(livros => this.setState({livros}))
+    .catch(function(error){
+      console.log("Erro na requisição")
+    })
+    .finally(function(){
+      console.log("Sempre Retorna")
+    })
+  }
   render(){
   return (
     <table className="tabela">
