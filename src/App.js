b32b43ffc2b6882c0d42e3651b5e46e68cfb6014
ligiadavilabozzi <ligia.bozzi@gmail.com>
2021-05-15 17:37:21 -0300

    import Component, create class; state; css and modify tbody

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, {Component} from 'react';
 import TabelaHead from './components/TabelaHead';
 import TabelaFoot from './components/TabelaFoot';
 import TabelaBody from './components/TabelaBody'
 
-
-function App() {
+class App extends Component {
+  state={
+    livros:[
+      {
+        id: "978-85-7522-632-2", 
+        titulo: "CSS Grid Layout",
+        autor: "Maurício Samy Silva"
+      },
+      {
+        id: "978-85-7522-677-3", 
+        titulo: "Node Essencial",
+        autor: "Ricardo R. Lacheta"
+      },
+      {
+        id: "978-85-7522-512-7", 
+        titulo: "Aprendendo Material Design",
+        autor: "Kyle Mew"
+      }
+    ]
+  };
+  render(){
   return (
     <table className="tabela">
       <TabelaHead />
-      <TabelaBody />
       <tabelaFoot />
-    </table>
-    
-  );
-}
+      <TabelaBody livros={ this.state.livros }/>
+    </table>  
+    );
+  }
+} 
 
 export default App;
