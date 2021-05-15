3172a3a11dfa3c06a16bac0b5547c0367d0bd204
ligiadavilabozzi <ligia.bozzi@gmail.com>
2021-05-15 17:03:16 -0300

    create components thead, tbody and tfoot

---
+++ src/components/TabelaFoot.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import TabelaBody from './TabelaBody';
+const TabelaFoot = () => (
+    <tfoot>
+        <tr>
+            <td colSpan="4">Este é o rodapé da tabela</td>
+        </tr>
+    </tfoot>
+);
+
+export default TabelaFoot; 
---
+++ src/components/TabelaHead.js
@@ -0,0 +1,15 @@
+import React from 'react'
+const TabelaHead = () =>(
+    <thead> 
+        <tr>
+            <th colSpan="4">Tabela de Livros</th>
+        </tr>
+        <tr>
+            <th>ISBN</th>
+            <th>Título</th>
+            <th>Autor</th>
+        </tr>
+    </thead>
+);
+
+export default TabelaHead; 
